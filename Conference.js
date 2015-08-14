2818391bb13dd557960b699a65af81da39563ed2
yanas <yana@jitsi.org>
2015-08-13 22:13:45 -0500

    Additions to the Conference interface

--- Conference.js
+++ Conference.js
@@ -1,5 +1,5 @@
 /**
- * Represents the conference.
+ * Creates a Conference object with the given name and properties.
  * @param name name of the conference.
  * @param options Object with properties / settings related to the conference that will be created.
  * @constructor
@@ -10,13 +10,28 @@ function Conference(name, options) {
 }
 
 /**
- * Requests media streams and returns them via the callback.
- * @param options Object with properties / settings related to the streams(audio, video, desktop)
- * that will be requested
+ * Joins the conference.
+ */
+Conference.prototype.join = function () {
+
+}
+
+/**
+ * Leaves the conference.
+ */
+Conference.prototype.leave = function () {
+
+}
+
+/**
+ * Creates the media streams and returns them via the callback.
+ * @param options Object with properties / settings defining which streams(Stream.AUDIO, Stream.VIDEO, Stream.DESKTOP)
+ * should be created or some additional configurations about resolution for example.
  * @param successCallback callback that will receive the streams.
  * @param errorCallback callback that will be called if accessing the media streams fail.
+ * @return an array of all created MediaStream-s
  */
-Conference.prototype.startLocalMedia = function (options, successCallback, errorCallback) {
+Conference.prototype.createMediaStreams = function (options, successCallback, errorCallback) {
 
 }
 
@@ -30,15 +45,6 @@ Conference.prototype.addEventListener = function (eventId, handler) {
 }
 
 /**
- * Receives notifications from another participants for commands / custom events(send by sendCommand method).
- * @param command {String} the name of the command
- * @param handler {Function} handler for the command
- */
-Conference.prototype.addCommandListener = function (command, handler) {
-
-}
-
-/**
  * Removes event listener
  * @param eventId the event ID.
  */
@@ -47,17 +53,20 @@ Conference.prototype.removeEventListener = function (eventId) {
 }
 
 /**
- * Removes command  listener
- * @param command {String}  the name of the command
+ * Receives notifications from another participants for commands / custom events(send by sendCommand method).
+ * @param command {String} the name of the command
+ * @param handler {Function} handler for the command
  */
-Conference.prototype.removeCommandListener = function (command) {
+Conference.prototype.addPresenceCommandListener = function (command, handler) {
 
 }
 
+
 /**
- * Leaves the conference.
+ * Removes command  listener
+ * @param command {String}  the name of the command
  */
-Conference.prototype.leave = function () {
+Conference.prototype.removePresenceCommandListener = function (command) {
 
 }
 
@@ -80,17 +89,40 @@ Conference.prototype.sendTextMessage = function (message) {
 }
 
 /**
- *
+ * Send presence command.
  * @param name the name of the command.
  * @param values Object with keys and values that will be send.
- * @param persistant if false the command will be sent only one time
+ * @param persistent if false the command will be sent only one time
  * otherwise it will be sent with every system message sent to the other participants.
  * @param successCallback will be called when the command is successfully send.
  * @param errorCallback will be called when the command is not sent successfully.
  */
-Conference.prototype.sendCommand = function (name, values, persistant, successCallback, errorCallback) {
+Conference.prototype.sendPresenceCommand = function (name, values, persistent, successCallback, errorCallback) {
 
 }
 
+/**
+ * Sets the display name for this conference.
+ * @param name the display name to set
+ */
+Conference.prototype.setDisplayName = function(name) {
+
+}
+
+/**
+ * Start desktop sharing. This will replace the video stream with the desktop sharing stream.
+ * @return Stream stream of type DESKTOP
+ */
+Conference.prototype.startDesktopSharing = function() {
+
+}
+
+/**
+ * Stop desktop sharing. This will replace the desktop stream with the video stream.
+ * @return Stream stream of type VIDEO
+ */
+Conference.prototype.endDesktopSharing = function() {
+
+}
 
 module.exports = Conference;
